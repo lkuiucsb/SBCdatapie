dea7fd5e1e3d9517e005a78d308b96d739ad6411
clnsmth <colin.smith@wisc.edu>
2019-06-14 19:32:29 -0500

    Rename copy_data_package()

--- R/data_package_copy.R
+++ R/data_package_copy.R
@@ -1,28 +1,36 @@
 #' Copy data package from temporary directory to user specified directory
 #'
 #' This function copies the data package from the temporary directory
-#' specified by paste0(tempdir(), '/data_package') and copies it to the user
+#' specified by paste0(tempdir(), '/data_package') to a user
 #' specified directory.
+#' 
+#' @usage
+#'   data_package_copy(
+#'     copy.dir
+#'   )
 #' 
 #' @param copy.dir
 #'   (character) Directory to which the data package will be copied.
 #'
 #' @return
 #'   (data package) Data package contents as parsed by \code{metajam}.
+#'   
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' # Download data package to temporary directory
-#' output <- read_data_archived(data.pkg.doi = 'doi:10.18739/A2DP3X')
+#' data_package_download('doi:10.18739/A2DP3X')
 #' 
 #' # Copy data package to user specified directory
-#' copy_data_package(copy.dir = '/Users/csmith/Desktop/data_packages')
+#' copy_data_package('/Users/csmith/Desktop/data_packages')
+#' 
+#' # Clean up
+#' data_package_remove()
 #' }
 #' 
-#' 
 
-copy_data_package <- function(copy.dir){
+data_package_copy <- function(copy.dir){
   
   # Check user specified directory
   
@@ -33,13 +41,17 @@ copy_data_package <- function(copy.dir){
   # Copy data package to "copy.dir"
   
   if (dir.exists(paste0(tempdir(), '/data_package'))){
+    
     file.copy(
       from = paste0(tempdir(), '/data_package/.'),
       to = copy.dir, 
       recursive = TRUE
     )
+    
   } else {
+    
     stop('No data package exists in the temporary directory.')
+    
   }
   
 }
