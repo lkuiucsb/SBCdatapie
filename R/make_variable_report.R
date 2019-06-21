a5cf03d979705ce019f4606ba0c5c27f3d10d32d
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2019-06-21 17:00:14 -0500

    better variable-wise report making

--- R/make_variable_report.R
+++ R/make_variable_report.R
@@ -1,28 +1,36 @@
-make_var_report <- function(varname) {
-  var <- df[[varname]]
-  plots <- if (is.numeric(var)) {
-    #print(varname[1])
+make_var_report <- function(space_cols = space_cols, entity_data, varname) {
+  
+  var <- entity_data[[varname]]
+  
+  if (is.numeric(var)) {
     xsummary <- make_numeric_variable_summary_df(var)
-    print(kable(xsummary))
-    #pander(xsummary)
-    x <- make_numeric_histogram(df, var, varname)
-    return(x)
+    x <- make_numeric_histogram(entity_data, var, varname)
+    plots <- list(xsummary=xsummary, x=x)
   } else if (is.factor(var) | is.character(var)) {
-    #print(varname[1])
     xsummary <- make_cat_variable_summary_df(var)
-    print(kable(xsummary, format = "pandoc"))
-    #pander(xsummary)
-    x <- make_categorical_histogram(df, var, varname)
-    return(x)
+    x <- make_categorical_histogram(entity_data, var, varname)
+    plots <- list(xsummary=xsummary, x=x)
+  } else {
+    plots <- NULL
   }
-  missing <- if (sum(xsummary$num_missing) > 0) {
-    #print("View where missing values occur in the data")
-    x <- make_missing_plot(var)
-    return(x)
-  }
+  
+  try(missing <- if (sum(plots$xsummary$num_missing) > 0) {
+    make_missing_plot(var)
+  })
   # spatial heatmap function for variable
   
-  space <- space_plot(space_cols, varname)
+  space <- space_plot(space_cols = space_cols, df = entity_data, var = varname)
   
-  return(list(plots, missing, space))
+  var_output <- list(
+    var_name = varname,
+    plots = plots,
+    missing = if (exists("missing"))
+      missing
+    else NULL,
+    space = if (exists("space"))
+      space
+    else NULL
+    )
+  
+  return(var_output)
 }
