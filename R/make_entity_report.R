3bd67b5744af58b099cfff917fc8d1f7591b5e88
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2019-06-24 15:38:57 -0500

    point to new path for static report template and output
    plus more commenting and fix typo in condition to use use_missing_codes

--- R/make_entity_report.R
+++ R/make_entity_report.R
@@ -5,10 +5,13 @@
 #'
 
 make_entity_report <- function(data_entity_list) {
+  
   begin_time <- Sys.time()
-  try(if ("attribute_metadata" %in% names(data_entity_list) & "missingValueCode" %in% names(data_entity_list[["data"]])) {
+  
+  # if there's missing codes specified in attribute metadata, then try and replace where these missing codes occur with NA's
+  if ("attribute_metadata" %in% names(data_entity_list) & "missingValueCode" %in% names(data_entity_list[["attribute_metadata"]])) {
     data_entity_list <- use_missing_code(data_entity_list)
-  })
+  }
   
   print_report <- function(i) {
     cat(i[["var_name"]])
@@ -18,18 +21,27 @@ make_entity_report <- function(data_entity_list) {
     i[["plots"]][["x"]]
   }
   
+  # append "File_Name" in summary metadata to report name
   report_name <-
     paste0("report_", data_entity_list[["summary_metadata"]][1, 2], ".html")
+  
+  # make entity-level report
   entity_data <- data_entity_list[["data"]]
   dt1 <- HackathonFunction1(entity_data)
-  space_cols <- space_detective(data_entity_list)
   try(dt2 <- HackathonFunction2(entity_data))
   try(dt3 <- HackathonFunction3(entity_data))
+  
+  # detect spatial information
+  space_cols <- space_detective(data_entity_list)
+  
+  
+  # loop over columns in entity data and make variable-level reports
   var_report <- lapply(colnames(entity_data), make_var_report, entity_data = entity_data, space_cols = space_cols)
   names(var_report) <- colnames(entity_data)
+  
   end_time <- Sys.time()
 
-  
+  # create environment for RMarkdown
   envir <-
     list(
       data_entity_list = data_entity_list,
@@ -37,7 +49,10 @@ make_entity_report <- function(data_entity_list) {
       df = entity_data,
       space_cols = space_cols,
       dt1 = dt1,
-      dt2 = dt2,
+      dt2 = if (exists("dt2"))
+        dt3
+      else
+        NULL,
       dt3 = if (exists("dt3"))
         dt3
       else
@@ -48,11 +63,17 @@ make_entity_report <- function(data_entity_list) {
       end_time = end_time    
       )
   
+  # set template path. Use the first one once this is in a package.
+  # template_path <- system.file("rmd", "static_report_template.Rmd", package = "dummypackagename")
+  template_path <- "./inst/rmd/static_report_template.Rmd"
+  
+  # set report output path. Outside of package context, this is in relation to the location of the static template
+  report_output_path <- "../../output/"
   
   try(rmarkdown::render(
-    input = "./R/static_report_template.Rmd",
+    input = template_path,
     output_format = "html_document",
-    output_file = report_name,
+    output_file = paste0(report_output_path, report_name),
     envir = envir
   ))
   #return(var_report)
