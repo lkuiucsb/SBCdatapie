eac3a5711491d4b17947de8d33b08dc687f1f852
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2019-06-25 13:11:06 -0500

    use_missing_code: update docs, argument name

--- R/use_missing_code.R
+++ R/use_missing_code.R
@@ -1,35 +1,37 @@
 #'
+#' Column-wise substitute NAs for missing codes, if specified in attribute metadata.
 #'
+#' @param entity_list (list) A list object containing information on a single data entity in metajam output format.
 #'
-#'
-#'
-#'
-#'
-#'
+#' @return If successful: a list object but with NAs in the right places in data. Otherwise, a list object identical to input.
+#' 
+#' @export
 
-use_missing_code <- function(data_entity_list) {
+use_missing_code <- function(entity_list) {
   
-  # if there's missing codes specified in attribute metadata, then try and replace where these missing codes occur with NA's
-  if ("attribute_metadata" %in% names(data_entity_list) & "missingValueCode" %in% names(data_entity_list[["attribute_metadata"]])) {
-    for (varname in colnames(data_entity_list[["data"]])) {
+  # check for attribute metadata and missingValueCode field
+  if ("attribute_metadata" %in% names(entity_list) & "missingValueCode" %in% names(entity_list[["attribute_metadata"]])) {
+    
+    # column-wise loop
+    for (varname in colnames(entity_list[["data"]])) {
       
       # temporarily excluding dateTime variables: method not applicable to POSIXct variables
-      if (subset(data_entity_list[["attribute_metadata"]],
+      if (subset(entity_list[["attribute_metadata"]],
                  attributeName == varname,
                  measurementScale) != "dateTime") {
         # get code
         code <-
-          subset(data_entity_list[["attribute_metadata"]],
+          subset(entity_list[["attribute_metadata"]],
                  attributeName == varname,
                  missingValueCode)
         
         # substitute where data matches code with NAs
-        is.na(data_entity_list[["data"]][[varname]]) <-
-          as.character(data_entity_list[["data"]][[varname]]) == as.character(code)
+        is.na(entity_list[["data"]][[varname]]) <-
+          as.character(entity_list[["data"]][[varname]]) == as.character(code)
       }
     }
     
-    return(data_entity_list)
+    return(entity_list)
   }
-  return(data_entity_list)
+  return(entity_list)
 }
